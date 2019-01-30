57a9523cc88cc1069bb46536e079ec474b5134c2
lrvc3 <rennylobo3@gmail.com>
2019-01-30 09:52:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/questionnaireapp/quesstionnaire_pg4.java
@@ -0,0 +1,13 @@
+package com.example.questionnaireapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quesstionnaire_pg4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quesstionnaire_pg4);
+    }
+}
---
+++ app/src/main/java/com/example/questionnaireapp/questionnaire_pg3.java
@@ -0,0 +1,13 @@
+package com.example.questionnaireapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class questionnaire_pg3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire_pg3);
+    }
+}
